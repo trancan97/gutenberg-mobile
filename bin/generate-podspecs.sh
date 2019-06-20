a7f6736772f6977621db8b471d983a153881b23c
etoledom <etoledom@icloud.com>
2019-06-20 11:53:30 +0200

    iOS: Adding new podsecs to third-party-podspecs

--- bin/generate-podspecs.sh
+++ bin/generate-podspecs.sh
@@ -16,6 +16,23 @@ node_modules/react-native/ReactCommon/yoga/yoga.podspec
 node_modules/react-native/third-party-podspecs/Folly.podspec
 node_modules/react-native/third-party-podspecs/DoubleConversion.podspec
 node_modules/react-native/third-party-podspecs/glog.podspec
+node_modules/react-native/React/React-Core.podspec
+node_modules/react-native/React/React-DevSupport.podspec
+node_modules/react-native/ReactCommon/cxxreact/React-cxxreact.podspec
+node_modules/react-native/ReactCommon/jsinspector/React-jsinspector.podspec
+node_modules/react-native/ReactCommon/jsiexecutor/React-jsiexecutor.podspec
+node_modules/react-native/ReactCommon/jsi/React-jsi.podspec
+node_modules/react-native/Libraries/WebSocket/React-RCTWebSocket.podspec
+node_modules/react-native/Libraries/fishhook/React-fishhook.podspec
+node_modules/react-native/Libraries/ActionSheetIOS/React-RCTActionSheet.podspec
+node_modules/react-native/Libraries/NativeAnimation/React-RCTAnimation.podspec
+node_modules/react-native/Libraries/Blob/React-RCTBlob.podspec
+node_modules/react-native/Libraries/Image/React-RCTImage.podspec
+node_modules/react-native/Libraries/Network/React-RCTNetwork.podspec
+node_modules/react-native/Libraries/LinkingIOS/React-RCTLinking.podspec
+node_modules/react-native/Libraries/Settings/React-RCTSettings.podspec
+node_modules/react-native/Libraries/Text/React-RCTText.podspec
+node_modules/react-native/Libraries/Vibration/React-RCTVibration.podspec
 EOP
 )
 
