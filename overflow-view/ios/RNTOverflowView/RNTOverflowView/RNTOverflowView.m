f4d64dcfe7667d4f0c2b1398742e39eb4e37fcb2
Chip Snyder <chip.snyder3@gmail.com>
2020-01-24 17:57:06 -0500

    Add RNTOverflow control component and get a successful hit test in iOS

--- overflow-view/ios/RNTOverflowView/RNTOverflowView/RNTOverflowView.m
+++ overflow-view/ios/RNTOverflowView/RNTOverflowView/RNTOverflowView.m
@@ -12,12 +12,32 @@
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     
-    for (UIView *subview in self.subviews) {
-        
-        NSLog(@"%@", NSStringFromCGRect(subview.frame));
+    CGPoint normalizedPoint = [self convertPoint:point toView:nil];
+    UIView *successfulHit = [self checkHitTest:normalizedPoint withEvent:event inView:self];
+
+    if(successfulHit) {
+        return successfulHit;
     }
     
     return [super hitTest:point withEvent:event];
+}
+
+- (UIView *)checkHitTest:(CGPoint)point withEvent:(UIEvent *)event inView:(UIView *)parent {
+    
+    for (UIView *subview in parent.subviews) {
+        
+        CGRect adjsutedFrame = subview.frame;
+        adjsutedFrame.origin = [parent convertPoint:subview.frame.origin toView:nil];
+        
+        if (CGRectContainsPoint(adjsutedFrame, point)) {
+            NSLog(@"HIT!");
+            return subview;
+        } else {
+            return [self checkHitTest:point withEvent:event inView:subview];
+        }
+    }
+    
+    return nil;
 }
 
 @end
