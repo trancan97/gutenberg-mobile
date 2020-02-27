e7d3ecbf98295c810c85510739d71a49166694ef
Chip Snyder <chip.snyder3@gmail.com>
2020-02-27 15:04:20 -0500

    Revert "Saving work for floaingtool bar"
    This reverts commit 0bcce84786686ed15ae54e2a0133ad10e87105e0.

--- react-native-overflow-view/android/src/main/java/org/wordpress/mobile/react_native_overflow_view/OverflowView.kt
+++ react-native-overflow-view/android/src/main/java/org/wordpress/mobile/react_native_overflow_view/OverflowView.kt
@@ -1,16 +1,14 @@
 package org.wordpress.mobile.react_native_overflow_view
 
 import android.content.Context
+import android.graphics.Point
+import android.graphics.Rect
 import android.view.MotionEvent
+import android.view.View
 import com.facebook.react.views.view.ReactViewGroup
 
 class OverflowView(context: Context) : ReactViewGroup(context) {
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val result = super.dispatchTouchEvent(ev)
-
-        return result
-    }
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
         val result = super.onInterceptTouchEvent(ev)
         return result
